fb7a6d290ab85465e654f7ba05024f502d9beb9a
TriteHexagon <tiagospace5@gmail.com>
2020-07-27 19:46:29 +0100

    Moves up to 80 (working)

--- data/pokemon/base_stats.asm
+++ data/pokemon/base_stats.asm
@@ -2,32 +2,39 @@ tmhm: MACRO
 ; used in data/pokemon/base_stats/*.asm
 _tms1 = 0 ; TM01-TM24 (24)
 _tms2 = 0 ; TM25-TM48 (24)
-_tms3 = 0 ; TM49-TM50 + HM01-HM07 + MT01-MT03 (12/24)
+_tms3 = 0 ; TM49-TM50 + HM01-HM07 + MT01-MT05 (12/24)
+_tms4 = 0 ; MT?
 rept _NARG
 	if DEF(\1_TMNUM)
 	if \1_TMNUM < 24 + 1
 _tms1 = _tms1 | (1 << ((\1_TMNUM) - 1))
 	elif \1_TMNUM < 48 + 1
 _tms2 = _tms2 | (1 << ((\1_TMNUM) - 1 - 24))
-	else
+	elif \1_TMNUM < 72 + 1
 _tms3 = _tms3 | (1 << ((\1_TMNUM) - 1 - 48))
+	else
+_tms4 = _tms3 | (1 << ((\1_TMNUM) - 1 - 72))
 	endc
 	else
 		fail "\1 is not a TM, HM, or move tutor move"
 	endc
 	shift
 endr
-rept 3 ; TM01-TM24 (24/24)
+rept 3 ; TM01-TM24
 	db _tms1 & $ff
 _tms1 = _tms1 >> 8
 endr
-rept 3 ; TM25-TM48 (24/24)
+rept 3 ; TM25-TM48
 	db _tms2 & $ff
 _tms2 = _tms2 >> 8
 endr
-rept 2 ; TM49-TM50 + HM01-HM07 + MT01-MT03 (12/16)
+rept 3 ; TM49-TM50 + HM01-HM07 + MT01-MT15
 	db _tms3 & $ff
 _tms3 = _tms3 >> 8
+endr
+rept 3 ; MT16-MT39
+	db _tms4 & $ff
+_tms4 = _tms4 >> 8
 endr
 ENDM
 
