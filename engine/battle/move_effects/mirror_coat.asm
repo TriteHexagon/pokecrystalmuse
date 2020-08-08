4a7ae013768ec8efc3cd161bb318617250859774
TriteHexagon <tiagospace5@gmail.com>
2020-08-08 19:35:09 +0100

    BF: Counter & Mirror Coar

--- engine/battle/move_effects/mirror_coat.asm
+++ engine/battle/move_effects/mirror_coat.asm
@@ -41,7 +41,7 @@ BattleCommand_MirrorCoat:
 	ld hl, wCurDamage
 	ld a, [hli]
 	or [hl]
-	ret z
+	jr z, .failed
 
 	ld a, [hl]
 	add a
@@ -57,4 +57,10 @@ BattleCommand_MirrorCoat:
 
 	xor a
 	ld [wAttackMissed], a
+	ret
+
+.failed
+	ld a, 1
+	ld [wEffectFailed], a
+	and a
 	ret
