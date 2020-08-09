e932fbcda7f36de43f59971bcab0caeaf8460ae8
TriteHexagon <tiagospace5@gmail.com>
2020-08-09 18:51:18 +0100

    Fixed Icon Problem

--- data/icon_pointers.asm
+++ data/icon_pointers.asm
@@ -6,39 +6,38 @@ IconPointers:
 	; dw DiglettIcon
 	; dw PikachuIcon
 	; dw StaryuIcon
-	dw FishIcon
-	dw BirdIcon
-	dw MonsterIcon
+	; dw FishIcon
+	; dw BirdIcon
+	; dw MonsterIcon
 	; dw ClefairyIcon
 	; dw OddishIcon
-	dw BugIcon
-	dw GhostIcon
+	; dw BugIcon
+	; dw GhostIcon
 	; dw LaprasIcon
-	dw HumanshapeIcon
-	dw FoxIcon
-	dw EquineIcon
-	dw ShellIcon
-	dw BlobIcon
-	dw SerpentIcon
+	; dw HumanshapeIcon
+	; dw FoxIcon
+	; dw EquineIcon
+	; dw ShellIcon
+	; dw BlobIcon
+	; dw SerpentIcon
 	; dw VoltorbIcon
 	; dw SquirtleIcon
 	; dw BulbasaurIcon
 	; dw CharmanderIcon
-	dw CaterpillarIcon
+	; dw CaterpillarIcon
 	; dw UnownIcon
 	; dw GeodudeIcon
-	dw FighterIcon
-	dw EggIcon
-	dw JellyfishIcon
-	dw MothIcon
-	dw BatIcon
+	; dw FighterIcon
+	; dw JellyfishIcon
+	; dw MothIcon
+	; dw BatIcon
 	; dw SnorlaxIcon
 	; dw HoOhIcon
 	; dw LugiaIcon
 	; dw GyaradosIcon
 	; dw SlowpokeIcon
 	; dw SudowoodoIcon
-	dw BigmonIcon
+	; dw BigmonIcon
 ;new icons
 	dw BulbasaurIcon
 	dw IvysaurIcon
@@ -291,3 +290,4 @@ IconPointers:
 	dw LugiaIcon
 	dw HoOhIcon
 	dw CelebiIcon
+	dw EggIcon
