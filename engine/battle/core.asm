7c8e5a7a98fcedc470339d31d5c874bd066e04d3
TriteHexagon <tiagospace5@gmail.com>
2020-08-11 21:04:05 +0100

    Level 100 mons don't gain exp

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -7118,6 +7118,15 @@ GiveExperiencePoints:
 	inc de
 	dec c
 	jr nz, .stat_exp_loop
+	;new code
+	pop bc
+	ld hl, MON_LEVEL
+	add hl, bc
+	ld a, [hl]
+	cp MAX_LEVEL
+	jp nc, .next_mon
+	push bc
+	;end new code
 	xor a
 	ldh [hMultiplicand + 0], a
 	ldh [hMultiplicand + 1], a
@@ -7415,11 +7424,33 @@ GiveExperiencePoints:
 	ld b, a
 	ld c, PARTY_LENGTH
 	ld d, 0
+	;new code
 .count_loop
+	push bc
+	push de
+	ld a, [wPartyCount]
+	cp c
+	jr c, .no_mon
+	ld a, c
+	dec a
+	ld hl, wPartyMon1Level
+	call GetPartyLocation
+	ld a, [hl]
+.no_mon
+	cp MAX_LEVEL
+	pop de
+	pop bc
+	jr nz, .gains_exp
+	srl b
+	ld a, d
+	jr .no_exp
+.gains_exp
+	;end new code
 	xor a
 	srl b
 	adc d
 	ld d, a
+.no_exp
 	dec c
 	jr nz, .count_loop
 	cp 2
