001a40d2ab3b29f1cc38b910043ad4a74ebe5e0a
TriteHexagon <tiagospace5@gmail.com>
2020-09-14 23:30:05 +0100

    Parties: Pryce, Clair

--- data/pokemon/base_stats/weepinbell.asm
+++ data/pokemon/base_stats/weepinbell.asm
@@ -7,7 +7,7 @@
 	db GRASS, POISON ; type
 	db 120 ; catch rate
 	db 151 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db NO_ITEM, SILVER_LEAF ; items
 	db GENDER_F50 ; gender ratio
 	
 	db 20 ; step cycles to hatch
